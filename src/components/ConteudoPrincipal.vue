2f3be5089755e2f0aa4be39d6890b4ebc612da21
Fred Rufino <frederico.diniz.rufino@gmail.com>
2024-02-16 11:18:34 -0200

    finishing styles

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import SuaListaVue from './SuaLista.vue';
+import SelecionarIngredientesVue from './SelecionarIngredientes.vue';
+
+    export default {
+   
+    components: { SelecionarIngredientesVue, SuaListaVue }
+}
+</script>
+
+<template>
+    <main class="conteudo-principal">
+        <SuaListaVue />
+        <SelecionarIngredientesVue />
+    </main>
+</template>
+
+<style scoped>
+    .conteudo-principal {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #FFFAF3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .conteudo-principal {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .conteudo-principal {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+
+</style>
